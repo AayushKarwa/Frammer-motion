95bb0be9df0827927e6b54df734cc2868db6b197
Ayush karwa <ayushkarwa18@gmail.com>
2024-08-02 01:18:18 +0530

    updated Background.jsx

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,7 +4,7 @@ function Background() {
   return (
     <>
     <div className='w-full h-screen bg-zinc-800'>
-      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[13vw] leading-none tracking-tighter font-semibold text-zinc-400'>Docs</h1>
+      <h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[13vw] leading-none tracking-tighter font-semibold text-zinc-400'>Drag</h1>
     </div>
     </>
 )
